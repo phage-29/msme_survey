a6865ebb42c9668596264cdbf178aead7b262f34
DAN <dace.phage@gmail.com>
2024-05-10 15:20:31 +0800

    added htaccess

--- reports.php
+++ reports.php
@@ -228,14 +228,14 @@ require_once("assets/components/templates/sidebar.php");
     </section>
     <script>
     $(document).ready(function() {
-        Swal.fire({
-            title: "Loading",
-            html: "Please wait...",
-            allowOutsideClick: false,
-            didOpen: function() {
-                Swal.showLoading();
-            },
-        });
+        // Swal.fire({
+        //     title: "Loading",
+        //     html: "Please wait...",
+        //     allowOutsideClick: false,
+        //     didOpen: function() {
+        //         Swal.showLoading();
+        //     },
+        // });
         $.ajax({
             url: 'fetch_data.php',
             method: 'GET',
